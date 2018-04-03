010be8a1d31ee3159d388ecd659b6d1671d80d66
Raghav <ragav.bpl@gmail.com>
2018-04-03 18:00:16 +0530

    converted to maven project

---
+++ src/main/java/test/test.java
@@ -0,0 +1,16 @@
+package test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.*;
+public class test {
+
+
+    @Test
+    public void testMethod() throws Exception{
+        System.out.println("hello world from testng");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\1022880\\Desktop\\Softwares\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        Thread.sleep(1000);
+        driver.close();
+    }
+}
